1d331b1c9f915af373b2df8eaebf332968df47d4
Nurjaxon <khudoyberdievnurjakhon@gmail.com>
2023-05-30 17:14:52 +0500

    add home work center

--- js/script.js
+++ js/script.js
@@ -6,14 +6,48 @@
 // console.log(num)
 
 
+             // uyga vazifa 1
 
-function calcAvarage (arr) {
-    let num = 0
-    for ( let i = 0; i < arr.length; i++) {
-        num = num + arr[i]
+
+// function calcAvarage (arr) {
+//     let num = 0
+//     for ( let i = 0; i < arr.length; i++) {
+//         num = num + arr[i]
         
-    }
-    return num /arr.length
-}
-let birorta = [4,6,7,8,9,9,9,12]
-console.log(calcAvarage(birorta))
+//     }
+//     return num /arr.length
+// }
+// let arr2 = [4,6,7,8,9,9,9,12]
+// console.log(calcAvarage(arr2))
+
+
+                 
+
+
+
+                  // uyga vazifa 2
+
+
+// function countVomels (firstName) {
+//     let summed =0
+//     for (let i = 0; i < firstName.length; i++)  {
+//         if(firstName[i] === 'a' || firstName[i] === 'o' || firstName[i] === 'e' || firstName[i] === 'i' || firstName[i] === 'u') {
+//             summed++
+//         } 
+
+//     }
+//     return summed
+// }
+// console.log(countVomels('Nurjaxon'))
+
+
+
+
+
+                     // uyga vazifa 3
+
+
+
+
+
+
